61777e922aabb3c65de11bfdc3b8d1210d8f13df
ahmadekhalili <ahmadkhalili2020@gmail.com>
2024-06-04 10:59:35 +0330

    Added .gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='onetomultipleimage',
-    version='1.0.5',
+    version='1.0.6',
     packages=['onetomultipleimage'],
     install_requires=["django", "djangorestframework", "pillow"],
     extras_require={
